48efe8b6e82f3e7133d8a14cd0495b3d9c4a7bbe
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2025-03-27 15:26:26 +0100

    update

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
-    public function exampleFunction()
+    public function exampleFunction():void
     {
         $foo = "Hello World"; // Errore: mancanza di spazio tra la dichiarazione e la variabile
         echo $foo;
@@ -19,7 +19,7 @@ class ExampleController extends Controller
         ];
     }
 
-    public function anotherFunction(){
+    public function anotherFunction(): void{
         $bar= "Goodbye"; // Errore: mancanza di spazi attorno all'operatore di assegnazione
     }
 
